ac204d0489b48fe843b4c1e11fb7d693a19f4f3e
Ben Hokanson-Fasig <bhokansonfasig@gmail.com>
2017-02-02 15:28:40 -0600

    Added file for basic job submission

---
+++ quick_submit.py
@@ -0,0 +1,44 @@
+#! /usr/bin/env python
+#
+# pycondor_submit.py
+# Script for submitting a basic script and arguments to HTCondor easily
+#
+#
+# Ben Hokanson-Fasig
+# Created   02/02/17
+# Last edit 02/02/17
+#
+
+
+from __future__ import division, print_function
+
+# Standard libraries
+import sys
+
+# Custom libraries
+from pycondor import Job
+
+
+# Get script and options
+script_file = sys.argv[1]
+start_index = script_file.rfind("/")+1
+extension_index = script_file.index(".")
+script_name = script_file[start_index:extension_index]
+options = sys.argv[2:]
+
+# Declare the error, output, log, and submit directories for Condor Job
+error = '/data/user/fasig/pycondor'
+output = '/data/user/fasig/pycondor'
+log = '/data/user/fasig/pycondor'
+submit = '/data/user/fasig/pycondor'
+
+# Setting up a PyCondor Job
+job = Job(script_name, script_file,
+          error=error, output=output,
+          log=log, submit=submit, verbose=2)
+
+# Adding arguments to job
+job.add_arg(" ".join(options))
+
+# Write all necessary submit files and submit job to Condor
+job.build_submit()
