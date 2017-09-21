8be4dd31acc8b51d19da74e8a06a9fa34f9e0e23
Ben Hokanson-Fasig <bhokansonfasig@gmail.com>
2017-09-21 17:39:57 -0500

    Added threshold plot script

---
+++ make_threshold_plots.py
@@ -0,0 +1,42 @@
+#! /usr/bin/env python
+#
+# make_threshold_plots.py
+# Script for submitting threshold plot script to HTCondor easily
+#
+#
+# Ben Hokanson-Fasig
+# Created   09/21/17
+# Last edit 09/21/17
+#
+
+
+from __future__ import division, print_function
+
+# Standard libraries
+import sys
+
+# Custom libraries
+from pycondor import Job
+
+
+# Get script and options
+script_file = "/home/fasig/scalable_radio_array/threshold_plotter.py"
+options = [script_file] + sys.argv[1:]
+
+# Declare the error, output, log, and submit directories for Condor Job
+error = '/data/user/fasig/pycondor'
+output = '/data/user/fasig/pycondor'
+log = '/data/user/fasig/pycondor'
+submit = '/data/user/fasig/pycondor'
+
+# Setting up a PyCondor Job
+job = Job("threshold_plotter", "/home/fasig/anaconda3/bin/python",
+          error=error, output=output,
+          log=log, submit=submit, verbose=2)
+
+# Adding arguments to job
+if len(options)>0:
+    job.add_arg(" ".join(options))
+
+# Write all necessary submit files and submit job to Condor
+job.build_submit()
