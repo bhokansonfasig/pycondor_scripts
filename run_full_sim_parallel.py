9b12305823ac3bcfc04b4efaf4865385c4abe110
Ben Hokanson-Fasig <bhokansonfasig@gmail.com>
2018-01-08 15:33:37 -0600

    Allow use of full_sim_ara script and different energies

--- run_full_sim_parallel.py
+++ run_full_sim_parallel.py
@@ -13,39 +13,59 @@
 from __future__ import division, print_function
 
 # Standard libraries
-import sys
+import argparse
 
 # Custom libraries
 from pycondor import Job
 
+default_energies = ["1e6", "2e6", "5e6",
+                    "1e7", "2e7", "5e7",
+                    "1e8", "2e8", "5e8",
+                    "1e9", "2e9", "5e9",
+                    "1e10"]
+
+# Parse command line arguments
+parser = argparse.ArgumentParser(description="""Script for submitting many
+                                 full_sim scripts to HTCondor easily.""")
+parser.add_argument('iterations', type=int,
+                    help="""Number of iterations to run (each full_sim script
+                         with different options submitted this many times)""")
+parser.add_argument('--energies', nargs='+', default=default_energies,
+                    help="""Energies over which to run simulations
+                         (defaults to 13 energies between 1e6 and 1e10)""")
+parser.add_argument('--ara', action="store_true",
+                    help="""If present, full_sim_ara script will be used
+                         instead of full_sim script""")
+parser.add_argument('--pass', action="store_true",
+                    help="""Does nothing. Useful for separating off which
+                         arguments should be passed to full_sim scripts""")
+parser.add_argument('options', nargs=argparse.REMAINDER,
+                    help="additional options passed on to full_sim scripts")
+
+args = parser.parse_args()
 
 # Get script and options
-script_file = "/home/fasig/scalable_radio_array/full_sim.sh"
+if args.ara:
+    script_file = "/home/fasig/scalable_radio_array/full_sim_ara.sh"
+else:
+    script_file = "/home/fasig/scalable_radio_array/full_sim.sh"
 
-energies = ["1e6", "2e6", "5e6",
-            "1e7", "2e7", "5e7",
-            "1e8", "2e8", "5e8",
-            "1e9", "2e9", "5e9",
-            "1e10"]
 
-iterations = int(sys.argv[1])
-zfill_amount = len(str(iterations-1))
+zfill_amount = len(str(args.iterations-1))
 
-options = sys.argv[2:]
+descriptive_name = "full_sim_"+args.options[0]
 
-descriptive_name = "full_sim_"+sys.argv[2]
-
-if "-n" in sys.argv:
-    descriptive_name += "_n"+sys.argv[sys.argv.index("-n")+1]
+if "-n" in args.options:
+    descriptive_name += "_n"+args.options[args.options.index("-n")+1]
 else:
     descriptive_name += "_n10"
 
-descriptive_name += "x"+str(iterations)
+descriptive_name += "x"+str(args.iterations)
 
 output_index = -1
-if "-o" in options:
-    output_index = options.index("-o") + 1
-    output_name = options[output_index]
+if "-o" in args.options:
+    output_index = args.options.index("-o") + 1
+    output_name = args.options[output_index]
 
 # Declare the error, output, log, and submit directories for Condor Job
 error = '/data/user/fasig/pycondor'
@@ -60,14 +80,14 @@ job = Job(descriptive_name, script_file,
         #   request_memory="5GB")
 
 # Adding arguments to job
-for energy in energies:
-    for i in range(iterations):
+for energy in args.energies:
+    for i in range(args.iterations):
         if output_index!=-1:
             replaced_name = output_name.replace("ENERGY", energy)
             replaced_name = replaced_name.replace("ITERATION",
                                                   str(i).zfill(zfill_amount))
-            options[output_index] = replaced_name
-        job.add_arg(" ".join([energy]+options))
+            args.options[output_index] = replaced_name
+        job.add_arg(" ".join([energy]+args.options))
 
 # Write all necessary submit files and submit job to Condor
 job.build_submit()
